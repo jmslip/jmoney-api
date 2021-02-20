179c1b2647dae528a862b2fdbcaf3aa0dfca88c2
Jorge Marcelo de Souza Junior <jms.slip@gmail.com>
2021-02-20 12:03:52 -0300

    Commit inicial

---
+++ src/main/java/com/jms/jmoneyapi/JmoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.jms.jmoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jms/jmoneyapi/JmoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jms.jmoneyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
